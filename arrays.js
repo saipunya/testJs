286ae127f9792ade463769ab435f3f2fdc156c59
saipunya <sumetchoorat@gmail.com>
2023-03-11 20:21:39 +0700

    add data

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let oddNumbers = [1,2,4,6,8,12,24]
+for (i=0; i<oddNumbers.length; i++) {
+    console.log(oddNumbers[i])
+}
+for (k of oddNumbers){
+    console.log(k)
+}
--- functions.js
+++ functions.js
@@ -36,15 +36,56 @@
 // }
 
 // default Parameters
-function total(){
-    let vat = 7
-    let prompt = require('prompt-sync')()
-    let q = parseInt(prompt('ระบุจำนวนปริมาณ >> '))
-    let p = parseInt(prompt('ระบุราคา >>'))
-    result = (q*p*(1+vat/100)).toFixed(2)
-    console.log('ราคาที่คุณจำเป็นจะต้องจ่ายเท่ากับ '+result) 
-}
-//total()
+// function total(){
+//     let vat = 7
+//     let prompt = require('prompt-sync')()
+//     let q = parseInt(prompt('ระบุจำนวนปริมาณ >> '))
+//     let p = parseInt(prompt('ระบุราคา >>'))
+//     result = (q*p*(1+vat/100)).toFixed(2)
+//     console.log('ราคาที่คุณจำเป็นจะต้องจ่ายเท่ากับ '+result) 
+// }
+// total()
+// let a = [2,4,6]
 
+// function summary (...nums){
+//     let sum = 0
+//     for (i=0; i<nums.length; i++){
+//         sum += nums[i]
+//     }
+//     return sum
+// }
+
+// console.log(summary(...a))
+
+//call back function
+// function msg (){
+//     console.log('test time')
+// }
+// setTimeout(()=>{
+//     console.log('test time')
+// },3000)
+
+
+// var add = function(x,y){
+//     return x + y
+// }
+// var calcu = function calculate(a,b,func){
+//     return func(a,b)
+// }
+
+// console.log(calcu(10,20,add))
+
+// var sayHello = (YouName)=>{
+//     console.log('hello '+ YouName)
+// }
+// let prompt = require('prompt-sync')()
+// sayHello(prompt('Please enter your name >> '))
+
+// var square = (x)=>{
+//     if(isFinite(x)){
+//         console.log(x**2) 
+//     }
+// }
+// square(prompt('Please enter width >> '))
 
 
