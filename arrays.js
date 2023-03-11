4458c953fc8a2adacad14399dda77cc6d359e963
saipunya <sumetchoorat@gmail.com>
2023-03-11 20:29:34 +0700

    add data 2

--- arrays.js
+++ arrays.js
@@ -1,7 +1,17 @@
 let oddNumbers = [1,2,4,6,8,12,24]
-for (i=0; i<oddNumbers.length; i++) {
-    console.log(oddNumbers[i])
-}
+// for (i=0; i<oddNumbers.length; i++) {
+//     console.log(oddNumbers[i])
+// }
+
+// for in บอกตำแหน่งของ array
+// for (x in oddNumbers){
+//     console.log(x)
+// }
+
+let a = 15 in oddNumbers
+console.log(a)
+
+// for of บอกค่าของ array
 for (k of oddNumbers){
     console.log(k)
 }
