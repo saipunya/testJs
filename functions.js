09728cd121e9f00388453fb35e0a6240ff6dc8ca
saipunya <sumetchoorat@gmail.com>
2023-03-11 14:51:49 +0700

    first commit

---
+++ functions.js
@@ -0,0 +1,37 @@
+// function sumRange(start, end){
+//     let sum = 0
+//     for (let i = start; i < end; i++){
+//         sum += i
+//     }
+//     console.log(`ผลรวมของ ${start} - ${end} = ${sum}`)
+// }
+
+// sumRange(6,10)
+
+// function sayHello (){
+//     for(var i=0; i<5;i++){
+//         console.log('hello')
+//     }
+    
+// }
+// function textPrint(){
+//     for(var i=1; i<5; i++){
+//         console.log('print File no : ' + i)
+//     }
+// }
+
+// function checkNumber(){
+//     let prompt = require('prompt-sync')()
+//     let x = parseInt(prompt('please enter a number>> '))
+//     if(x < 50){
+//         console.log('คุณกรอกข้อมูลน้อยเกินไป')
+//         return
+//     }
+//     let result = (x % 2==0) ? `${x} เป็นเลขคู่` :`${x} เป็นเลขคี่`
+//     return result
+// }
+// console.log(checkNumber())
+
+
+
+
---
+++ index.js
@@ -0,0 +1,51 @@
+// let prompt = require('prompt-sync')()
+// const rating = (parseInt(prompt('please enter your rating 1-5 >>')))
+// switch (rating) {
+//     case 1 :
+//         console.log('very bad')
+//         break
+//     case 2 :
+//         console.log('bad')
+//         break
+//     case 3 :
+//         console.log('good')
+//         break
+//     case 4 :
+//         console.log('very good')
+//         break
+//     case 5 :
+//         console.log('excellent')
+//         break
+//     default :
+//         console.log('none value')
+// }
+// let sum = 0
+// for(i=1; i<=10;i++){
+//     sum += i
+//     if(i >=7){
+//         break
+//     }
+// }
+// console.log(sum)
+// let x = 2
+// while(x < 10){
+//     console.log(x)
+//     x++
+// }
+
+// let prompt = require('prompt-sync')()
+// let validcode = 12345
+// let pass = ''
+// while(pass != validcode){
+//     pass = prompt('please enter code')
+// }
+// console.log(`สุดยอดแล้ว code คือ ${validcode}`)
+
+// let prompt = require('prompt-sync')()
+// let x = 0
+// do{
+//     x = parseInt(prompt('please enter your code >>>'))
+// }while(x <= 99)
+// console.log('thank you')
+
+
